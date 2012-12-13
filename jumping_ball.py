483c1333a3b8ed8ce04822e01205eb86a498fe6c
Thomas Koch <thomas@koch.ro>
2012-12-13 11:39:39 +0100

    use ball image, add y direction, sleep

--- jumping_ball.py
+++ jumping_ball.py
@@ -1,24 +1,30 @@
 import pygame
 import sys
+import time # fuer sleep
 
 pygame.init()
 
 screen = pygame.display.set_mode( (640, 480) )
 
-surface = pygame.Surface( (10, 10) )
-surface.fill(pygame.Color("red"))
+surface = pygame.image.load("/usr/share/doc/python-pygame/tut/intro/ball.gif")
 rect = surface.get_rect()
 
 x_direction = 1
+y_direction = 1
 
 while True:
   if rect.right > 640 or rect.left < 0:
     x_direction = x_direction * -1
 
-  rect = rect.move(x_direction, 0)
+  if rect.bottom > 480 or rect.top < 0:
+    y_direction = y_direction * -1
+
+  rect = rect.move(x_direction, y_direction)
   screen.fill((0,0,0))
   screen.blit(surface, rect)
   pygame.display.flip()
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT: sys.exit()
+
+  time.sleep(0.0018)
