8904d7cb06aafdb953d551abfea2cfdef7021c26
Thomas Koch <thomas@koch.ro>
2012-12-13 11:31:42 +0100

    rectangle that moves

--- jumping_ball.py
+++ jumping_ball.py
@@ -1,9 +1,23 @@
 import pygame
+import sys
 
 pygame.init()
 
-# (640, 480) ist ein "Tuppel"
 screen = pygame.display.set_mode( (640, 480) )
 
+surface = pygame.Surface( (10, 10) )
+surface.fill( pygame.Color("red") )
+
+rect = surface.get_rect()
+
 while True:
-  print "nothing"
+  rect = rect.move( 1, 0 )
+
+  # neu: Bildschirm wieder schwarz machen
+  screen.fill( (0, 0, 0) )
+  screen.blit(surface, rect)
+
+  pygame.display.flip()
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT: sys.exit()
