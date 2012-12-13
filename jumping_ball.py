8fb7c6885a23add7f8b3f9d8f7d5f45d95f950db
Thomas Koch <thomas@koch.ro>
2012-12-13 11:35:37 +0100

    clean screen before move

--- jumping_ball.py
+++ jumping_ball.py
@@ -1,23 +1,26 @@
 import pygame
-import sys
+import sys # fuer sys.exit()
 
 pygame.init()
 
 screen = pygame.display.set_mode( (640, 480) )
 
+# neu: surface = Oberflaeche, kann spaeter auch ein Bild sein
 surface = pygame.Surface( (10, 10) )
 surface.fill( pygame.Color("red") )
 
+# neu: Rechteck hat Position und Groesse
 rect = surface.get_rect()
 
 while True:
-  rect = rect.move( 1, 0 )
+  rect = rect.move( 1, 0 ) # neues, verschobenes Rechteck
 
-  # neu: Bildschirm wieder schwarz machen
-  screen.fill( (0, 0, 0) )
-  screen.blit(surface, rect)
+  # Zeichne Oberflaeche in Rechteck
+  screen.blit( surface, rect )
 
+  # aktualisiere Bildschirm
   pygame.display.flip()
 
+  # neu: Auf Quit event des Fensters reagieren.
   for event in pygame.event.get():
     if event.type == pygame.QUIT: sys.exit()
