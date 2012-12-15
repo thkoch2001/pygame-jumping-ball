65a2e53116b0fd157a2218b9eb72ae78135d2ea6
Thomas Koch <thomas@koch.ro>
2012-12-15 20:15:12 +0100

    use ball sprite class

--- jumping_ball.py
+++ jumping_ball.py
@@ -6,29 +6,49 @@ pygame.init()
 
 screen = pygame.display.set_mode( (640, 480) )
 
-surface = pygame.image.load("/usr/share/doc/python-pygame/tut/intro/ball.gif")
-rect = surface.get_rect()
-
 sound = pygame.mixer.Sound("/usr/share/pyshared/pygame/examples/data/whiff.wav")
 
-x_direction = 1
-y_direction = 1
+class Ball(pygame.sprite.Sprite):
+
+    def __init__(self, area):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("/usr/share/doc/python-pygame/tut/intro/ball.gif").convert()
+        self.rect = self.image.get_rect()
+        self.area = area
+        self.x_direction = 1
+        self.y_direction = 1
+
+    def toggle_x_direction(self):
+        self.x_direction *= -1
+
+    def toggle_y_direction(self):
+        self.y_direction *= -1
+
+    def update(self):
+        global sound
+
+        if self.rect.left < self.area.left or self.rect.right > self.area.right:
+            self.toggle_x_direction()
+            sound.play()
+
+        if self.rect.top < self.area.top or self.rect.bottom > self.area.bottom:
+            self.toggle_y_direction()
+            sound.play()
+
+        self.rect = self.rect.move(self.x_direction, self.y_direction)
+
+ball = Ball(pygame.display.get_surface().get_rect())
+allsprites = pygame.sprite.RenderPlain((ball))
 
 while True:
-  if rect.right > 640 or rect.left < 0:
-    x_direction = x_direction * -1
-    sound.play()
 
-  if rect.bottom > 480 or rect.top < 0:
-    y_direction = y_direction * -1
-    sound.play()
+    allsprites.update()
+    screen.fill((0,0,0))
+    allsprites.draw(screen)
 
-  rect = rect.move(x_direction, y_direction)
-  screen.fill((0,0,0))
-  screen.blit(surface, rect)
-  pygame.display.flip()
+    pygame.display.flip()
 
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT: sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
 
-  time.sleep(0.0018)
+    time.sleep(0.0018)
