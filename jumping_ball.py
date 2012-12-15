a780ed670e2241b880566cdcc651ca981cd60842
Thomas Koch <thomas@koch.ro>
2012-12-15 20:40:10 +0100

    randomly place new balls on keypress

--- jumping_ball.py
+++ jumping_ball.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-import time # fuer sleep
+import time
+import random
 
 pygame.init()
 
@@ -8,15 +9,25 @@ screen = pygame.display.set_mode( (640, 480) )
 
 sound = pygame.mixer.Sound("/usr/share/pyshared/pygame/examples/data/whiff.wav")
 
+def randomdirection():
+    return random.choice((-1, 1))
+
+def randomspeed():
+    return random.randint(1, 4)
+
 class Ball(pygame.sprite.Sprite):
 
     def __init__(self, area):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("/usr/share/doc/python-pygame/tut/intro/ball.gif").convert()
         self.rect = self.image.get_rect()
+        self.rect = self.rect.move(random.randint(0, area.right - 1 - self.rect.right),
+                                   random.randint(0, area.bottom - 1 -self.rect.bottom))
         self.area = area
-        self.x_direction = 1
-        self.y_direction = 1
+        self.x_direction = randomdirection()
+        self.y_direction = randomdirection()
+        self.x_speed = randomspeed()
+        self.y_speed = randomspeed()
 
     def toggle_x_direction(self):
         self.x_direction *= -1
@@ -35,7 +46,7 @@ class Ball(pygame.sprite.Sprite):
             self.toggle_y_direction()
             sound.play()
 
-        self.rect = self.rect.move(self.x_direction, self.y_direction)
+        self.rect = self.rect.move(self.x_direction * self.x_speed, self.y_direction * self.y_speed)
 
 ball = Ball(pygame.display.get_surface().get_rect())
 allsprites = pygame.sprite.RenderPlain((ball))
@@ -49,6 +60,9 @@ while True:
     pygame.display.flip()
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            allsprites.add(Ball(pygame.display.get_surface().get_rect()))
 
     time.sleep(0.0018)
